c9b21bb04ff13616217a3237e985cd4e38e422e2
naman0867 <ngarg9003@gmail.com>
2026-06-15 22:35:56 +0530

    Fixing issues

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,16 @@
-const Hero = () => {
-  return <div>Hero Working</div>;
+import Hero from "../components/Hero";
+import { useAuth } from "../context/authContext";
+import LandingNav from "../components/LandingNav";
+import Footer from "../components/Footer";
+import Features from "../components/Features";
+import Payments from "../components/Payments";
+import CustomerLogos from "../components/CustomerLogos";
+
+const Home = () => {
+const { isAuthenticated } = useAuth();
+
+return ( <div className="bg-dark min-h-screen"> <LandingNav /> <Hero /> <Features /> <Payments /> <CustomerLogos /> <Footer /> </div>
+);
 };
 
-export default Hero;
+export default Home;
